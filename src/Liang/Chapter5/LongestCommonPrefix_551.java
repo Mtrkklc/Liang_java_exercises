4ef3c893832283a3c9dc2ec02a156aefdc63f9c1
Mustafa Tarık KILIÇ <kilicmustafatarik@gmail.com>
2025-05-21 18:04:59 +0300

    Loop soruları bitti.

--- src/Liang/Chapter5/LongestCommonPrefix_551.java
+++ src/Liang/Chapter5/LongestCommonPrefix_551.java
@@ -1,7 +1,41 @@
 package Liang.Chapter5;
 
+import java.util.Scanner;
+
 public class LongestCommonPrefix_551 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first a string: ");
+        String firstString = input.nextLine();
+
+        System.out.print("Enter the second a string: ");
+        String secondString = input.nextLine();
+
+        String commonPrefix = "";
+        String shortString = "";
+
+        if (firstString.length() > secondString.length()) {
+            shortString = secondString;
+        } else {
+            shortString = firstString;
+        }
+
+        for (int i = 0, j = 0; i < shortString.length(); i++, j++) {
+            char firstStringChar = firstString.charAt(i);
+            char secondStringChar = secondString.charAt(j);
+
+            if (firstStringChar == secondStringChar) {
+                commonPrefix += firstStringChar;
+            }else{
+                System.out.print(firstString + " and " + secondString + " have no common prefix.");
+                break;
+            }
+
+        }
+        if(!commonPrefix.isEmpty()){
+            System.out.print("The common prefix is: " + commonPrefix);
+        }
+
 
     }
 }
